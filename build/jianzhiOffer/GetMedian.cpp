6c446525f9d0e9c55c2d1f5cec96f60a2d3d7ddc
zhangzhaoru <914413583@qq.com>
2020-01-26 22:23:02 +0800

    Commit a new code

---
+++ build/jianzhiOffer/GetMedian.cpp
@@ -0,0 +1,54 @@
+//动态维护数据流中的中位数
+#include<iostream>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+priority_queue<int,vector<int>,less<int>> p;
+//从大到小进行排列
+priority_queue<int,vector<int>,greater<int>> q;
+//从小到大进行排列
+
+void Insert(int num ){
+    if(p.empty() || num<=p.top())
+            p.push(num);
+        else
+            q.push(num);
+        if(p.size()==q.size()+2){
+            q.push(p.top());
+            p.pop();
+        }
+        if(p.size()==q.size()-1){
+            p.push(q.top());
+            q.pop();
+        }
+}
+
+double GetMeddian(){
+    if(p.size()==q.size()){
+        return (p.top()+q.top())/2.0;
+    }else{
+        return p.top();
+    }
+}
+
+int main(){
+    
+    cout<<"Please input the Count : ";
+    int count;
+    cin>>count;
+
+    for(int i=0;i<count;i++){
+        int tempNum;
+        cout<<"Please input a new number: ";
+        cin>>tempNum;
+        Insert(tempNum);
+        cout<<"the current data stream 's meddian is : "
+        <<GetMeddian()<<endl;
+        cout<<endl;
+        cout<<endl;
+    }
+    system("pause");
+    return 0;
+}
