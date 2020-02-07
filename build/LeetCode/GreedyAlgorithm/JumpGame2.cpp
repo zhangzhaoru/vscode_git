720e19ba7444c15f0fe520c970b70ef724c4e9fd
zhangzhaoru <914413583@qq.com>
2020-02-07 10:56:51 +0800

    Commit a new code

--- build/LeetCode/GreedyAlgorithm/JumpGame2.cpp
+++ build/LeetCode/GreedyAlgorithm/JumpGame2.cpp
@@ -1,21 +1,20 @@
 //求最少需要几次跳动
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int jumpMin(vector<int> &nums){
-    if(nums.size()<2)
-        return 0;
+int jump(vector<int> &nums) {
+    if (nums.size() < 2) return 0;
     int jumpMin = 1;
     int curMaxIndex = nums[0];
     int preMaxIndex = nums[0];
-    for(int i = 0;i<nums.size();i++){
-        if(i>curMaxIndex){
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > curMaxIndex) {
             jumpMin++;
             curMaxIndex = preMaxIndex;
         }
-        if(preMaxIndex<i+nums[i]){
-            preMaxIndex = i+nums[i];
+        if (preMaxIndex < i + nums[i]) {
+            preMaxIndex = i + nums[i];
         }
     }
     return jumpMin;
@@ -23,9 +22,9 @@ int jumpMin(vector<int> &nums){
 
 int main() {
     vector<int> nums = {2, 3, 1, 1, 4};
-    int res = jumpMin(nums);
-    cout<<"The min jumpMin count is : ";
-    cout<<res<<endl;
+    int res = jumpe(nums);
+    cout << "The min jumpMin count is : ";
+    cout << res << endl;
     system("pause");
     return 0;
 }
