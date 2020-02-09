c00a85fdb49a7912ba472a532c1204f534b6d102
zhangzhaoru <914413583@qq.com>
2020-02-09 15:40:09 +0800

    commit a new code

--- build/LeetCode/RecursionBacktracking/makesquare.cpp
+++ build/LeetCode/RecursionBacktracking/makesquare.cpp
@@ -15,24 +15,24 @@ bool makesquare(vector<int> &nums){
     if(sum%4!=0){
         return false;
     }
-    sort(nums.rbegin(),nums.rend());
+    int target = sum/4;
     int backet[4] = {0};
-    return generate(0,nums,sum/4,backet);
+    return generate(0,nums,target,backet);  
 }
 
 bool generate(int i,vector<int> &nums,int target,int backet[]){
     if(i>=nums.size()){
-        bool condition1 = backet[0] == target;
-        bool condition2 = backet[1] == target;
-        bool condition3 = backet[2] == target;
-        bool condition4 = backet[3] == target;
+        bool condition1 = (backet[0]==target);
+        bool condition2 = (backet[1]==target);
+        bool condition3 = (backet[2]==target);
+        bool condition4 = (backet[3]==target);
         return condition1&&condition2&&condition3&&condition4;
     }
     for(int j = 0;j<4;j++){
         if(backet[j]+nums[i]>target){
             continue;
         }
-        backet[j]+=nums[i];
+        backet[j] += nums[i];
         if(generate(i+1,nums,target,backet)){
             return true;
         }
