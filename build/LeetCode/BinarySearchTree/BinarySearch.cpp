fb14dc15a8f3b4580522854e3225ad1ed4198fe2
zhangzhaoru <914413583@qq.com>
2020-02-22 23:18:11 +0800

    commit a new code

--- build/LeetCode/BinarySearchTree/BinarySearch.cpp
+++ build/LeetCode/BinarySearchTree/BinarySearch.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: Zhang Zhaoru
  * @Since: 2020-02-22 18:21:42
- * @LastTime: 2020-02-22 20:53:21
+ * @LastTime: 2020-02-22 23:17:52
  * @LastAuthor: Zhang Zhaoru
  * @FilePath: \vscode_git\build\LeetCode\BinarySearchTree\BinarySearch.cpp
  * @Description: ??????????????????
@@ -9,9 +9,14 @@
 
 #include <stdio.h>
 #include <iostream>
-#include<vector>
-#include<sstream>
+#include <sstream>
+#include <vector>
 using namespace std;
+
+void Swap(int *&pointer1, int *&pointer2);
+void qSort(int left, int right, vector<int> &v);
+istream &operator>>(istream &input1, vector<int> &v);
+ostream &operator<<(ostream &output, vector<int> &v);
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -54,6 +59,79 @@ void BST_insert(TreeNode *node, TreeNode *&insert_node) {
     }
 }
 
+// ???????????
+TreeNode *findParent(TreeNode *node, int value, TreeNode *&parent) {
+    while (node) {
+        if (node->val == value) {
+            break;
+        }
+        parent = node;
+        if (value < node->val) {
+            node = node->left;
+        } else {
+            node = node->right;
+        }
+    }
+    return node;
+}
+
+// ??????(????????????????)
+TreeNode *findSuccessor(TreeNode *node, TreeNode *&parent) {
+    parent = node;
+    TreeNode *ptr = node->right;
+    while (ptr->left) {
+        parent = ptr;
+        ptr = ptr->left;
+    }
+    return ptr;
+}
+
+// ???????????(?)??
+void deletenode(TreeNode *parent, TreeNode *node) {
+    if (node->val < parent->val) {
+        if (node->left && !node->right) {
+            parent->left = node->left;
+        } else if (!node->left && node->right) {
+            parent->left = node->right;
+        } else {
+            parent->left = NULL;
+        }
+    } else if (node->val > parent->val) {
+        if (node->left && !node->right) {
+            parent->right = node->left;
+        } else if (!node->left && node->right) {
+            parent->right = node->right;
+        } else {
+            parent->right = NULL;
+        }
+    }
+}
+
+TreeNode *deleteNode(TreeNode *root, int key) {
+    TreeNode *parent = NULL;
+    TreeNode *node = findParent(root, key, parent);
+    if (!node) {
+        return root;
+    }
+    if (node->left && node->right) {
+        TreeNode *successor = findSuccessor(node, parent);
+        deletenode(parent, successor);
+        node->val = successor->val;
+        return root;//???????????????????
+    }
+    if (parent) {
+        deletenode(parent, node);
+        return root;
+    }
+    // ????????????
+    if (node->left) {
+        node = node->left;
+    } else {
+        root = node->right;
+    }
+    return root;
+}
+
 void midOrderPrint(TreeNode *root) {
     if (!root) {
         return;
@@ -74,81 +152,77 @@ void preOrderInsert(const vector<int> &nums, vector<TreeNode *> &nodeVec,
     preOrderInsert(nums, nodeVec, mid + 1, end);
 }
 
-
 TreeNode *CreateBST(vector<int> &nums) {
     if (nums.size() == 0) {
         return NULL;
     }
     vector<TreeNode *> nodeVec;
-    preOrderInsert  (nums, nodeVec, 0, nums.size() - 1);
+    qSort(0, nums.size() - 1, nums);
+    preOrderInsert(nums, nodeVec, 0, nums.size() - 1);
     for (int i = 1; i < nodeVec.size(); i++) {
         BST_insert(nodeVec[0], nodeVec[i]);
     }
     return nodeVec[0];
 }
 
-void Swap(int*& pointer1,int *&pointer2){
+void Swap(int *&pointer1, int *&pointer2) {
     int temp = *pointer1;
     *pointer1 = *pointer2;
     *pointer2 = temp;
 }
 
-void qSort(int left,int right,vector<int> &v){
-    if(left>right)
-        return;
-    int i,j,base;
+void qSort(int left, int right, vector<int> &v) {
+    if (left > right) return;
+    int i, j, base;
     i = left;
     j = right;
     base = v[left];
-    while(i<j){
-        while(v[j]>=base && i<j){
+    while (i < j) {
+        while (v[j] >= base && i < j) {
             j--;
         }
-        while(v[i]<=base && i<j){
+        while (v[i] <= base && i < j) {
             i++;
         }
-        if(i<j){
+        if (i < j) {
             int *pointer1 = &v[i];
             int *pointer2 = &v[j];
-            Swap(pointer1,pointer2);
+            Swap(pointer1, pointer2);
         }
-
     }
     v[left] = v[i];
     v[i] = base;
-    qSort(left,i-1,v);
-    qSort(i+1,right,v);
+    qSort(left, i - 1, v);
+    qSort(i + 1, right, v);
 }
 
-
-
-istream& operator >>(istream &input1,vector<int> &v){
+istream &operator>>(istream &input1, vector<int> &v) {
     string s;
-    getline(input1,s);
+    getline(input1, s);
     stringstream ss(s);
     int x;
-    while(ss>>x){
+    while (ss >> x) {
         v.push_back(x);
     }
     return input1;
 }
 
-ostream &operator<<(ostream &output,vector<int> &v){
-    for(auto i = v.begin();i<v.end();i++){
-        output<<*i;
-        if(i<v.end()-1){
-            output<<" ";
+ostream &operator<<(ostream &output, vector<int> &v) {
+    for (auto i = v.begin(); i < v.end(); i++) {
+        output << *i;
+        if (i < v.end() - 1) {
+            output << " ";
         }
     }
     return output;
 }
 
-
-
 int main() {
     vector<int> data = {62, 88, 58, 47, 35, 73, 51, 99, 37, 93, 36, 39, 42, 62};
-    qSort(0,data.size()-1,data);
     TreeNode *root = CreateBST(data);
+    midOrderPrint(root);
+    cout<<endl;
+    root = deleteNode(root,99);
     midOrderPrint(root);
     cout << endl;
     return 0;
---
+++ build/LeetCode/BinarySearchTree/ConvertBST.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: Zhang Zhaoru
+ * @Since: 2020-02-22 22:51:36
+ * @LastTime: 2020-02-22 22:51:37
+ * @LastAuthor: Zhang Zhaoru
+ * @FilePath: \vscode_git\build\LeetCode\BinarySearchTree\ConvertBST.cpp
+ * @Description:
+ */
